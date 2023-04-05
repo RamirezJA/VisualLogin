2bb2d4a80f30a464e995864fcdbe25fba08681b0
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2023-04-05 14:57:15 -0700

    nav 0.2

--- my-app/src/pages/navbar.js
+++ my-app/src/pages/navbar.js
@@ -1,11 +1,20 @@
 import React from "react"
 import styles from "../styles/navbar.module.css"
+import Link from "next/link"
 export default function navbar() {
   return (
     <div className={styles.nav}>
       <ul className={styles.menu}>
-        <li>Ai Grade</li>
-        <li>About</li>
+        <li>
+          <Link className={styles.link} href='/'>
+            Ai Grader
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href='/about'>
+            About
+          </Link>
+        </li>
       </ul>
     </div>
   )
