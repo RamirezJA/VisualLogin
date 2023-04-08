cbe417d28a03960e17a783e6320662a0b23fa973
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2023-04-08 16:30:33 -0700

    +text box to openAPI 0.1

--- my-app/src/pages/api/openai.js
+++ my-app/src/pages/api/openai.js
@@ -1,19 +1,22 @@
-import { OpenAIApi, Configuration } from "openai"
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration)
-
 export default async (req, res) => {
   if (req.body.prompt !== undefined) {
-    const completion = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
-      prompt: `${req.body.prompt}`,
-    })
-
-    res.status(200).json({ text: `${completion.data.choices[0].text}` })
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "234a6cbe3emshe13fc43e8c18567p1dc548jsn1792f2f2506b",
+        "X-RapidAPI-Host": "openai80.p.rapidapi.com",
+      },
+      body: '{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":`${req.body.prompt}`}]}',
+    }
+    const response = await fetch(
+      "https://openai80.p.rapidapi.com/chat/completions",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response.choices[0].message.content))
+      .catch((err) => console.error(err))
+    res.end()
   } else {
     res.status(400).json({ text: "No prompt provided." })
   }
