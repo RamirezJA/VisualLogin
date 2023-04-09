cd69830664d86dd53281c77af981b56bf09e3df4
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2023-04-09 16:29:39 -0700

    test

--- my-app/src/pages/chatbox.js
+++ my-app/src/pages/chatbox.js
@@ -3,9 +3,7 @@ import styles from "../styles/chatbox.module.css"
 export default function chatbox() {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        Go ahead, ask the AI - no question too tough, no answer too tricky! 🧘🏽
-      </div>
+      <div className={styles.title}>Go ahead, ask the AI 🧘🏽</div>
       {/*
       <div className={styles.box}>
         <div className={styles.btn}>Upload</div>
