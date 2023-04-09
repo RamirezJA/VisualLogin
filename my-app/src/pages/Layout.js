1312a5421f56abb812c5bb639aec065d0c571639
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2023-04-09 00:25:22 -0700

    footer fix to btm

--- my-app/src/pages/Layout.js
+++ my-app/src/pages/Layout.js
@@ -3,14 +3,15 @@
 import Navbar from "./navbar"
 import Footer from "./footer"
 import Chatbox from "./chatbox"
+import style from "../styles/site.module.css"
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={style.site}>
       <Navbar />
       <Chatbox />
-      <main>{children}</main>
+      <main className={style.main}>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
--- my-app/src/pages/footer.js
+++ my-app/src/pages/footer.js
@@ -1,8 +1,10 @@
 import styles from "../styles/footer.module.css"
 export default function footer() {
   return (
-    <footer className={styles.footer}>
-      <div>Made with ❤️ in Los Angeles</div>
-    </footer>
+    <div className={styles.container}>
+      <footer className={styles.footer}>
+        <div>Made with ❤️ in Los Angeles</div>
+      </footer>
+    </div>
   )
 }
